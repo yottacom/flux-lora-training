d5060ecc929fd25b60d528440120a735cc24991e
hamza-m-farooqi <hamzamfarooqi@gmail.com>
2024-10-08 09:50:55 +0000

    docker file

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+# Use the NVIDIA CUDA base image
+FROM nvidia/cuda:12.0.1-runtime-ubuntu22.04
+
+# Set environment variables to avoid issues with prompts
+ENV HF_TOKEN=""
+
+
+# Install apt packages
+RUN apt-get update && apt-get install -y \
+    ffmpeg \
+    libomp-dev \
+    build-essential \
+    libssl-dev \
+    libffi-dev \
+    libgl1-mesa-glx \
+    python3-pip \
+    gdb \
+    valgrind \
+    git
+
+# Update pip to the latest version
+RUN python3 -m pip install --upgrade pip
+
+# Copy all files into the /app directory
+COPY . /app
+
+# Set the working directory
+WORKDIR /app
+
+# Install Python packages
+RUN python3 -m pip install -r requirements.txt
+
--- main.py
+++ main.py
@@ -5,10 +5,9 @@ from server.utils import (
     save_images_and_generate_metadata,
     generate_config_file,
     webhook_response,
-    list_dir
 )
 from server.request_processor import process_request
-from server import server_settings 
+
 
 def train(training_request_dict: dict):
     job=None
@@ -43,11 +42,9 @@ def train(training_request_dict: dict):
         training_request.example_prompts = example_prompts
         config_file_path = generate_config_file(training_request)
         training_request.config_file = config_file_path
-        
+
         print("Config File generated successfully!", config_file_path)
         job = Job(job_id=job_id, job_request=training_request, job_epochs=10)
-        print("File Structure is Following : ")
-        print(list_dir(server_settings.BASE_DIR))
         process_request(job)
         webhook_response(
             job.job_request.webhook_url, True, 200, "Job Completed", job.dict()
@@ -62,39 +59,39 @@ def parse_args():
     parser.add_argument('--training_request', type=str, required=True, help='Training request JSON string')
     return parser.parse_args()
 
-if __name__ == "__main__":
-    args = parse_args()
-    # Convert the JSON string back to a dictionary
-    training_request_dict = json.loads(args.training_request)
+# if __name__ == "__main__":
+#     args = parse_args()
+#     # Convert the JSON string back to a dictionary
+#     training_request_dict = json.loads(args.training_request)
     
-    # Call the train function with the parsed dictionary
-    train(training_request_dict)
+#     # Call the train function with the parsed dictionary
+#     train(training_request_dict)
 
-# training_request_dict = {
-#     "job_id": "818cfa9f-8a94-4123-b15c-a0ad097df7d6",
-#     "lora_name": "Irfan",
-#     "webhook_url": "http://34.170.162.109:8000/api/train_lora_gcloud_webhook/",
-#     "images_urls": [
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/5_nrFxoHy.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/8_gqxczDK.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/12_Ehg7LdQ.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/13_83IOj8I.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/14_8YdvYMl.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/15_acbmB79.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/22_6NwVFcf.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/25_MVz9VEl.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/26_RWXkqth.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/30_s1X8bgP.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/34_q44pwlG.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/36_S2nOmpF.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/41_gUwvrQy.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/43_izBe2Fn.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/46_3NYQWjf.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/47_nTcZgDy.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/48_68j55v4.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/49_L6Y9j7H.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/50_KlJ4G0k.jpg",
-#         "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/56_BVFsEb8.jpg"
-#     ],
-# }
-# train(training_request_dict)
+training_request_dict = {
+    "job_id": "818cfa9f-8a94-4123-b15c-a0ad097df7d6",
+    "lora_name": "Irfan",
+    "webhook_url": "http://34.170.162.109:8000/api/train_lora_gcloud_webhook/",
+    "images_urls": [
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/5_nrFxoHy.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/8_gqxczDK.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/12_Ehg7LdQ.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/13_83IOj8I.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/14_8YdvYMl.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/15_acbmB79.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/22_6NwVFcf.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/25_MVz9VEl.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/26_RWXkqth.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/30_s1X8bgP.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/34_q44pwlG.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/36_S2nOmpF.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/41_gUwvrQy.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/43_izBe2Fn.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/46_3NYQWjf.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/47_nTcZgDy.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/48_68j55v4.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/49_L6Y9j7H.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/50_KlJ4G0k.jpg",
+        "https://boothybooth.s3.amazonaws.com/lora_images/IrfanFlux/56_BVFsEb8.jpg"
+    ],
+}
+train(training_request_dict)
